a7b2d05d28ea187e5dc2b95a3f4e6433a24a8d1c
Zjoswaa <joshuavdjagt@gmail.com>
2025-10-15 13:47:32 +0200

    Added tables
    Parking lots, parking sessions, reservations

--- app/db/base.py
+++ app/db/base.py
@@ -3,4 +3,7 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 from db.models import user
+from db.models import parking_lot
+from db.models import parking_session
+from db.models import reservation
 from db.models import vehicle
