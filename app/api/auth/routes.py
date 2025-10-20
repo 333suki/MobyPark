c59b30b6b2ed92dc879df0849d0a280fc2aa9fd3
Telmo Claro <telmoclaro@proton.me>
2025-10-20 16:39:05 +0200

    By running main.py you are directly redirected to the API docs

--- app/api/auth/routes.py
+++ app/api/auth/routes.py
@@ -2,10 +2,29 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from db.database import SessionLocal
 from db.models.user import User
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 import bcrypt
 from datetime import date
+import uuid
 
+
+router = APIRouter(prefix="/auth", tags=["Authorization"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def hash_password(password: str) -> str:
+    """Hash a password using bcrypt"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+# http://127.0.0.1:8000/auth/
+@router.get("/")
+async def root(db: Session = Depends(get_db)):
+    return db.query(User).all()
 
 class UserCreate(BaseModel):
     username: str
@@ -30,25 +49,7 @@ class UserResponse(BaseModel):
     active: bool
 
     class Config: # USE THIS FOR SQLAlchemy MODELS!
-        orm_mode = True
-
-router = APIRouter(prefix="/auth", tags=["Authorization"])
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def hash_password(password: str) -> str:
-    """Hash a password using bcrypt"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
-# http://127.0.0.1:8000/auth/
-@router.get("/")
-async def root(db: Session = Depends(get_db)):
-    return db.query(User).all()
+        from_attributes = True
 
 @router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def register_user( user: UserCreate, db: Session = Depends(get_db),):
@@ -95,3 +96,55 @@ async def register_user( user: UserCreate, db: Session = Depends(get_db),):
     db.refresh(db_user)
 
     return db_user
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserLoginResponse(BaseModel):
+    id: int
+    username: str
+    name: str
+    email: str
+    phone: str
+    role: str
+    created_at: date
+    birth_year: int
+    active: bool
+    token: str
+
+    class Config: # USE THIS FOR SQLAlchemy MODELS!
+        from_attributes = True
+
+@router.post("/login", response_model=UserLoginResponse, status_code=status.HTTP_201_CREATED)
+async def login_user( user: UserLogin, db: Session = Depends(get_db),):
+    """
+    logs in a user
+    """
+
+    # Checks if a user exists
+    db_user = db.query(User).filter(User.username == user.username).first()
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username doesn't exist"
+        )
+
+    hashed_password = hash_password(user.password)
+
+    # Verifies password
+    if hashed_password == db_user.password:
+        token = str(uuid.uuid4())
+        return UserLoginResponse(
+            id=db_user.id,
+            username=db_user.username,
+            name=db_user.name,
+            email=db_user.email,
+            phone=db_user.phone,
+            role=db_user.role,
+            created_at=db_user.created_at,
+            birth_year=db_user.birth_year,
+            active=db_user.active,
+            token=token
+        )
+    return None
