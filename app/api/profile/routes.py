b920e6c2c7443add74aa3f4efd1d79724b7945e1
333suki <sogaandmomayez@gmail.com>
2025-11-17 15:51:02 +0100

    Update profile endpoint

--- app/api/profile/routes.py
+++ app/api/profile/routes.py
@@ -7,6 +7,8 @@ from app.api.profile.schemas import UpdateProfileBody
 from app.db.database import SessionLocal
 from app.util.jwt_authenticator import JWTAuthenticator, TokenMissingError, TokenInvalidError, TokenExpiredError
 
+from app.db.models.user import User
+
 router = APIRouter(prefix="/profile", tags=["Profile"])
 
 def get_db():
@@ -40,7 +42,71 @@ async def update_profile(request: Request, body: Optional[UpdateProfileBody] = B
             detail=str(e)
         )
 
-    # user_id: int = user_info.get("sub")
-    # user_role: str = user_info.get("role")
+    # Check if there is no body provided
+    if body is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="No request body"
+        )
+    
+    # Check if the body is empty
+    if all(value is None for value in body.dict().values()):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Empty request body"
+        )
+    
+    user_id: int = user_info.get("sub")
+    user_role: str = user_info.get("role")
 
+    user: User | None = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+        
+    # Username validation check and update
+    if body.username is not None:
+        if db.query(User).filter(User.username == body.username).first() is not None:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Username already exists"
+            )
+        else:
+            user.username = body.username
+
+    # Password update
+    if body.password is not None:
+        user.password = body.passowrd
+
+    # Name update
+    if body.name is not None:
+        user.name = body.name
+
+    # Email validation and update
+    if body.email is not None:
+        if db.query(User).filter(User.email == body.email).first() is not None:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Email already exists"
+            )
+        else:
+            user.email = body.email
+
+    # phone validation check and update
+    if body.phone is not None:
+        if db.query(User).filter(User.phone == body.phone).first() is not None:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Phone number already taken"
+            )
+        else:
+            user.phone = body.phone
+
+    # Birthyear update
+    if body.birth_year is not None:
+            user.birth_year = body.birth_year
+
+    db.commit()
     return { "message": "Profile Updated successfully" }
