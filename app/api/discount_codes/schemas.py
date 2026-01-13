f89f657146294eeed21e6f37fc211221f5b2c005
Zjoswaa <joshuavdjagt@gmail.com>
2026-01-13 18:55:38 +0100

    Added discount codes model and endpoints

---
+++ app/api/discount_codes/schemas.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class DiscountCodeResponse(BaseModel):
+    id: int
+    code: str
+    percentage: int
+    type: str
+    used: bool
+
+class CreateDiscountCodeBody(BaseModel):
+    code: Optional[str] = None
+    percentage: Optional[int] = None
+    type: Optional[str] = None
+    used: Optional[bool] = None
+
+class UpdateDiscountCodeBody(BaseModel):
+    code: Optional[str] = None
+    percentage: Optional[int] = None
+    type: Optional[str] = None
+    used: Optional[bool] = None
